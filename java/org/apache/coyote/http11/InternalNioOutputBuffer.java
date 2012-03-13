e8090d137ea7c08acf886802403a75aa019d73c8
Nabil Benothman <nabil.benothman@gmail.com>
2012-03-13 14:25:41 +0100

    Non-Blocking OPs

--- java/org/apache/coyote/http11/InternalNioOutputBuffer.java
+++ java/org/apache/coyote/http11/InternalNioOutputBuffer.java
@@ -116,12 +116,19 @@ public class InternalNioOutputBuffer extends AbstractInternalOutputBuffer {
 	 * @return the number of bytes written, -1 in case of errors
 	 */
 	private int blockingWrite(ByteBuffer buffer, long timeout, TimeUnit unit) {
-		System.out
-				.println("****** " + getClass().getName() + "#blockingWrite(timeout = "+timeout+") -> START ******");
+		System.out.println("****** " + getClass().getName() + "#blockingWrite(timeout = " + timeout
+				+ ") -> START ******");
 		try {
 			long wrTimeout = timeout > 0 ? timeout : Integer.MAX_VALUE;
 			System.out.println("*** WRITE_TIMEOUT = " + wrTimeout + ", TIME_UNIT = " + unit
 					+ " ***");
+			
+			
+			byte b[] = new byte[buffer.limit()];
+			buffer.get(b);
+			System.out.println("To Write : " + new String(b));
+			
+			buffer.flip();
 			return this.channel.writeBytes(buffer, wrTimeout, unit);
 		} catch (Exception e) {
 			log.warn("An error occurs when trying a blocking write");
